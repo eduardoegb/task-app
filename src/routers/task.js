8515793b2a2baa5860bbf4e92a4ed70f21d1f57b
Eduardo Gonzalez <eduardoegb@gmail.com>
2020-06-23 16:52:24 -0400

    Sorting, pagination and filtering

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,8 +22,28 @@ router.post('/tasks', auth, async (req, res) => {
 
 // Get all tasks
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const sorting = req.query.sortBy.split(':');
+    sort[sorting[0]] = sorting[1] === 'desc' ? -1 : 1;
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate();
     res.send(req.user.tasks);
   } catch (error) {
     res.status(500).send(error);
